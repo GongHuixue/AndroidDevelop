35126c2be4eac8dd73a2ecb06fce8ddad0a0cc4b
huixue.gong <huixue.gong@tpv-tech.com>
2018-01-24 13:40:47 +0800

    Add BroadcastReceiver Test

---
+++ app/src/main/java/com/example/huixuegong/androiddevelop/broadcast/DynamicActivity.java
@@ -0,0 +1,16 @@
+package com.example.huixuegong.androiddevelop.broadcast;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.huixuegong.androiddevelop.R;
+
+public class DynamicActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dynamic);
+    }
+}
