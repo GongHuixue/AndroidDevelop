ab725c631836cd233b43c94f1a703eb7e477cade
huixue.gong <huixue.gong@tpv-tech.com>
2018-01-26 16:29:12 +0800

    add messenager test

---
+++ app/src/main/java/com/example/huixuegong/androiddevelop/icp/messenger/MessengerService.java
@@ -0,0 +1,58 @@
+package com.example.huixuegong.androiddevelop.icp.messenger;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
+
+public class MessengerService extends Service {
+    public final static String TAG = MessengerService.class.getSimpleName();
+
+    public MessengerService() {
+    }
+
+    private final Messenger mMessenger = new Messenger(new MessengerHandler());
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mMessenger.getBinder();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private static class MessengerHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MyConstants.MSG_FROM_CLIENT:
+                    Log.i(TAG, "receive msg from Client:" + msg.getData().getString("msg"));
+                    Messenger client = msg.replyTo;
+                    Message relpyMessage = Message.obtain(null, MyConstants.MSG_FROM_SERVICE);
+                    Bundle bundle = new Bundle();
+                    bundle.putString("reply", "yes，i had received your message, will reply you later");
+                    relpyMessage.setData(bundle);
+                    try {
+                        client.send(relpyMessage);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/huixuegong/androiddevelop/icp/messenger/MyConstants.java
@@ -0,0 +1,10 @@
+package com.example.huixuegong.androiddevelop.icp.messenger;
+
+/**
+ * Created by huixue.gong on 2018/1/26.
+ */
+
+public class MyConstants {
+    public static final int MSG_FROM_CLIENT = 0;
+    public static final int MSG_FROM_SERVICE = 1;
+}
